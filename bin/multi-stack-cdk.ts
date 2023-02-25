8a041da685afa46319aaf925679ecb3e7d4c895d
Enrique M. Talavera <kiquetal@gmail.com>
2023-02-25 09:53:44 -0300

    Add example how to retrieve from variable from a file

--- bin/multi-stack-cdk.ts
+++ bin/multi-stack-cdk.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { MultiStackCdkStack } from '../lib/multi-stack-cdk-stack';
 
-const app = new cdk.App({
+/*const app = new cdk.App({
 context:{
    prod: {
          account:'12321312',
@@ -16,25 +16,15 @@ context:{
    }   
 }		       
 });
+
+*/
+const app = new cdk.App();
 const stackDev = new MultiStackCdkStack(app, 'MultiStackCdkStackDev', {
 	stackName:'multistack-dev',
 	env:{
               region: app.node.tryGetContext('dev')['region'],
               account:app.node.tryGetContext('dev')['account']
 	}
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
 console.log('dev-account',stackDev.account);
 const stackProd = new MultiStackCdkStack(app,'MultiStackCdkStackProd',{
